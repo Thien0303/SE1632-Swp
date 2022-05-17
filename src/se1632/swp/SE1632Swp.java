7d550bff9a2ae80639cdca05bbcce54bd3b1a8ca
Thien0303 <lethien0330@gmail.com>
2022-05-17 15:24:26 +0700

    1st time to play with git

---
+++ src/se1632/swp/SE1632Swp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se1632.swp;
+
+/**
+ *
+ * @author Thien Le
+ */
+public class SE1632Swp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Thien Le");
+    }
+    
+}
